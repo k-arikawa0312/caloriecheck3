586f1dfd74f3f1ecb9d904fac12ff97845b978d7
k-arikawa0312 <s1F102402710@iniad.org>
2024-10-23 12:02:58 +0900

    いったんコミット

--- hooks/useFirebaseAuth.js
+++ hooks/useFirebaseAuth.js
@@ -1,39 +1,39 @@
-import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential } from 'firebase/auth'
-import * as Google from 'expo-auth-session/providers/google'
-import * as WebBrowser from 'expo-web-browser'
-import { useEffect } from 'react'
-import { auth } from '@/firebase'
-import Constants from 'expo-constants'
+// import { GoogleAuthProvider, onAuthStateChanged, signInWithCredential } from 'firebase/auth'
+// import * as Google from 'expo-auth-session/providers/google'
+// import * as WebBrowser from 'expo-web-browser'
+// import { useEffect } from 'react'
+// import { auth } from '@/firebase'
+// import Constants from 'expo-constants'
 
-WebBrowser.maybeCompleteAuthSession()
-const useAuth = () => {
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    webClientId: `${process.env.EXPO_PUBLIC_WEB_CLIENT_ID}`,
-  })
-  onAuthStateChanged(auth, (user) => {
-    if (user != null) {
-      console.log('We are authenticated now!')
-    }
+// WebBrowser.maybeCompleteAuthSession()
+// const useAuth = () => {
+//   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+//     webClientId: `${process.env.EXPO_PUBLIC_WEB_CLIENT_ID}`,
+//   })
+//   onAuthStateChanged(auth, (user) => {
+//     if (user != null) {
+//       console.log('We are authenticated now!')
+//     }
 
-    // Do other things
-  })
-  const signOut = () => {
-    auth.signOut()
-  }
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { id_token } = response.params
+//     // Do other things
+//   })
+//   const signOut = () => {
+//     auth.signOut()
+//   }
+//   useEffect(() => {
+//     if (response?.type === 'success') {
+//       const { id_token } = response.params
 
-      const getCredential = GoogleAuthProvider.credential
-      const credential = getCredential(id_token)
-      signInWithCredential(auth, credential)
-    }
-  }, [response])
+//       const getCredential = GoogleAuthProvider.credential
+//       const credential = getCredential(id_token)
+//       signInWithCredential(auth, credential)
+//     }
+//   }, [response])
 
-  return { request, promptAsync, signOut }
-}
+//   return { request, promptAsync, signOut }
+// }
 
-export default useAuth
+// export default useAuth
 // import { GoogleSignin } from '@react-native-google-signin/google-signin';
 // import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 // import { auth } from '../firebase';
