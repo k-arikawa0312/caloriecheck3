c9257a19713094460f0a625f23efe2c29d219cb4
k-arikawa0312 <s1F102402710@iniad.org>
2024-10-28 15:49:52 +0900

    update validation

--- utils/addUserValidationSchema.ts
+++ utils/addUserValidationSchema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const addUserValidationSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
+    email: z.string().email({message:"emailが正しくありません"}).nonempty({message:"emailは必須です"}),
+    password: z.string().min(6,{message:"パスワードは6文字以上で入力してください"}),
     name: z.string().min(1),
     age: z.number().min(0),
     weight: z.number().min(0),
