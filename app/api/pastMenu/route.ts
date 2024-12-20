140c55dc612bb75693230b18359b77b1ac1c8b75
k-arikawa0312 <s1F102402710@iniad.org>
2024-12-20 23:25:48 +0900

    pastmenu add api

--- app/api/pastMenu/route.ts
+++ app/api/pastMenu/route.ts
@@ -19,3 +19,30 @@ export const GET = async (req:Request, res:Response) => {
         await prisma.$disconnect();
     }
 }
+
+export const POST = async (req:Request, res:Response) => {
+    try{
+        await main();
+        const {menuTitle,amount,ateAt,timeZone, calorie,protein,fat,carbohydrate,fiber,salt} = await req.json();
+        const pastMenu = await prisma.menu.create({
+            data: {
+                menuTitle: menuTitle,
+                amount: amount,
+                ateAt: ateAt,
+                timeZone: timeZone,
+                calories: calorie,
+                protein: protein,
+                fat: fat,
+                carbs: carbohydrate,
+                fiber: fiber,
+                salt: salt
+            }
+        });
+        return Response.json({messeage: "success", data: pastMenu});
+    }catch(e){
+        console.log(e);
+        return Response.json({messeage: "error"});
+    }finally{
+        await prisma.$disconnect();
+    }
+}
